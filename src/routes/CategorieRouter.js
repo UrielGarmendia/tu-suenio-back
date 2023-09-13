14b38a7db5f29d45407e8196c374140e9571b7da
PedroLor1a <pedroloria003@gmail.com>
2023-09-13 20:01:54 -0300

    routes Categorie

---
+++ src/routes/CategorieRouter.js
@@ -0,0 +1,43 @@
+const { Router } = require("express");
+const {
+  getAllCategorie,
+  getCategorieByName,
+  createCategorie,
+} = require("../controllers/CategorieController");
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const { name } = req.query;
+  try {
+    const categories = name
+      ? await getCategorieByName(name)
+      : await getAllCategorie();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(400).send("error:" + error.message);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const categorieId = await getAllCategorie();
+    const searchId = categorieId.filter((c) => c.id == id);
+    res.status(200).json(searchId);
+  } catch (error) {
+    res.status(400).send("error:" + error.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name } = req.body;
+  try {
+    const createDb = await createCategorie(name);
+    res.status(200).json(createDb);
+  } catch (error) {
+    res.status(400).send("error:" + error.message);
+  }
+});
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
+const CategorieRouter = require("./CategorieRouter");
 const router = Router();
-const productRouter = require("./productRouter");
-const userRouter = require("./userRouter");
 
-router.use("/products", productRouter);
-router.use("/user", userRouter);
+router.use("/categorie", CategorieRouter);
 
 module.exports = router;
