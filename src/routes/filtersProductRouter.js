88176cfa22fd4021e77c07028472d3654640bb3b
SanteZ97 <santiago_zucchi@hotmail.com>
2023-09-19 16:54:33 -0300

    filtros combinados

--- src/routes/filtersProductRouter.js
+++ src/routes/filtersProductRouter.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProductsByCategory } = require("../controllers/Products/filters");
-const { getProductsBySize } = require('../controllers/Products/filters');
+const { getProductsByCategory, getProductsBySize, getProductsByCategoryAndSize } = require("../controllers/Products/filters");
 
 router.get("/categorie/:categoryId", async (req, res) => {
   const { categoryId } = req.params;
@@ -36,6 +35,31 @@ router.get('/size/:size', async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Error al obtener productos por tamaño.' });
+  }
+});
+
+router.get("/combined/:categoryId/:size", async (req, res) => {
+  const { categoryId, size } = req.params; // Obtén los valores de categoryId y size desde los parámetros de consulta (query parameters)
+
+  try {
+    let products;
+
+    if (categoryId && size) {
+      // Si se proporcionan tanto categoryId como size, realiza una búsqueda combinada
+      products = await getProductsByCategoryAndSize(categoryId, size);
+    } else {
+      // Si no se proporcionan ni categoryId ni size, devuelve un mensaje de error
+      return res.status(400).json({ message: "Debe proporcionar al menos un filtro (categoryId o size)." });
+    }
+
+    if (products.length === 0) {
+      return res.status(404).json({ message: "No se encontraron productos que coincidan con los filtros proporcionados." });
+    }
+
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al obtener productos." });
   }
 });
 
